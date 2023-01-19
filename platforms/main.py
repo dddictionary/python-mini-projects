ac1403f1ce19af3a7c7fc754030b4baa467539bd
Abrar Habib <abrarhabib285@gmail.com>
2023-01-19 18:23:10 -0500

    works for 3 inputs

--- platforms/main.py
+++ platforms/main.py
@@ -11,8 +11,8 @@ def contains(x: float, platform: list[int]) -> bool:
     return platform[1] <= x <= platform[2]
 
 
-assert (contains(20.0, [100, 5, 10])) == False  # Works
-assert (contains(7.3, [100, 5, 10])) == True  # Works
+assert(contains(20.0, [100, 5, 10])) == False  # Works
+assert(contains(7.3, [100, 5, 10])) == True  # Works
 
 
 platforms = []
@@ -24,19 +24,22 @@ supports = 0
 #   platforms.append(list(map(int, input().split())))
 
 
-platforms = [[50, 50, 90],
-             [40, 40, 80],
-             [30, 30, 70],
-             [20, 20, 60],
-             [10, 10, 50]]
-platforms.sort(key=lambda x: x[0], reverse=True)
+# platforms = [[1, 5, 10],[3, 1, 5],[5, 3, 7]]
+
+platforms = [[50, 50, 90], [40, 40, 80],[30, 30, 70],[20, 20, 60],[10, 10, 50]]
+platforms.sort(reverse=True)
 print(platforms)
 
-
 for idx, platform in enumerate(platforms):
-    supports += platform[0] * 2
-    if idx > 1:
-        if contains(platform[1], platforms[idx-1]) or contains(platform[2], platforms[idx-1]):
-            supports -= abs(platforms[idx-1][0] - platform[0])
+	y = platform[0]
+	x1 = platform[1]
+	x2 = platform[2]
+	supports += y*2
+	if idx >= 1:
+		prev = platforms[idx-1]
+		if contains(x1,prev):
+			supports -= abs(prev[0]-y)
+		elif contains(x2,prev):
+			supports -= abs(prev[0]-y)
 
 print(supports)
